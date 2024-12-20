2b04f54ca32d2520b9aa4f77afa41089f77698fc
KuroiZZ <habiltatar01@gmail.com>
2024-12-20 19:39:39 +0300

    CreateUser function's developments started.

--- src/main/java/org/example/SessionSystem/Session.java
+++ src/main/java/org/example/SessionSystem/Session.java
@@ -1,8 +1,32 @@
 package org.example.SessionSystem;
 
+import org.example.Connection;
+import org.example.User.User;
+
+import java.sql.*;
+
 public class Session
 {
-    public void CreateUser(){}
+    static public void CreateUser(User user) throws SQLException
+    {
+        java.sql.Connection connection = (java.sql.Connection) DriverManager.getConnection(Connection.url, Connection.user, Connection.password);
+
+        String query = "INSERT INTO user (User_Id, User_Name, First_Name, Last_Name, Password, Role) VALUES(?,?,?,?,?,?);";
+
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1, user.getUserId());
+        stmt.setString(2, user.getUserName());
+        stmt.setString(3, user.getFirstName());
+        stmt.setString(4, user.getLastName());
+        stmt.setString(5, user.getPassword());
+        stmt.setString(6, user.getRole());
+
+        stmt.execute();
+
+        stmt.close();
+        connection.close();
+
+    }
 
     public void LogIn(){}
 
