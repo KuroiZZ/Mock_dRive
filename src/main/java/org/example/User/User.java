22b568eac9d830cdaabfcb154be76c967e1cdbe0
KuroiZZ <habiltatar01@gmail.com>
2024-12-20 19:38:51 +0300

    Constructor and getters created.

--- src/main/java/org/example/User/User.java
+++ src/main/java/org/example/User/User.java
@@ -9,6 +9,16 @@ public class User
     private String Password;
     private Role Role;
 
+    public User(String userId, String userName, String firstName, String lastName, String password, org.example.User.Role role)
+    {
+        UserId = userId;
+        UserName = userName;
+        FirstName = firstName;
+        LastName = lastName;
+        Password = password;
+        Role = role;
+    }
+
     public void ChangeUserName(){}
 
     public void SendChangePasswordRequest(){}
@@ -19,4 +29,32 @@ public class User
 
     public void ShareFileWithTeam(){}
 
+    public String getUserId()
+    {
+        return UserId;
+    }
+
+    public String getUserName()
+    {
+        return UserName;
+    }
+
+    public String getFirstName()
+    {
+        return FirstName;
+    }
+
+    public String getLastName()
+    {
+        return LastName;
+    }
+
+    public String getPassword()
+    {
+        return Password;
+    }
+
+    public String getRole() {
+        return Role.toString();
+    }
 }
