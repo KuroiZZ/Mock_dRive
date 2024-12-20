15dd9a4866e1e39fabbfafdf95b55828ebf32ca0
KuroiZZ <habiltatar01@gmail.com>
2024-12-20 19:39:12 +0300

    Constructor created.

--- src/main/java/org/example/User/Admin.java
+++ src/main/java/org/example/User/Admin.java
@@ -2,6 +2,11 @@ package org.example.User;
 
 public class Admin extends User
 {
+    public Admin(String userId, String userName, String firstName, String lastName, String password, org.example.User.Role role)
+    {
+        super(userId, userName, firstName, lastName, password, role);
+    }
+
     public void ChangeSelectedUserStorageLimit(){}
 
     public void ResponseUserChangePasswordRequest(){}
