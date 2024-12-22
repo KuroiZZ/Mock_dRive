c6bec86900c5dcec2281bd87356f6c1ff689956a
KuroiZZ <habiltatar01@gmail.com>
2024-12-22 12:26:33 +0300

    User example created for testing new created functions.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import org.example.SessionSystem.Session;
+import org.example.User.Admin;
+import org.example.User.Role;
+import org.example.User.User;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        User user1 = new User("3","KuroiZ", "Habil", "Tatarogullari", "495693");
+
+        try
+        {
+            Session.CreateUser(user1);
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }
