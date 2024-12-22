c0aa8de32155801d435d05ff38bbaf2f1c5f4472
KuroiZZ <habiltatar01@gmail.com>
2024-12-22 18:38:02 +0300

    RegisterMenuInitializer function created.

--- src/main/java/org/example/GUI/GUI_Elements.java
+++ src/main/java/org/example/GUI/GUI_Elements.java
@@ -95,13 +95,101 @@ public class GUI_Elements
         LogInPanel.add(LogIn_Button, setConstraints(1,1,0,2));
         //--
 
-        Window.add(LogInPanel, BorderLayout.CENTER);
+        Window.add(LogInPanel);
         Window.setVisible(true);
     }
 
     public static void InitializeRegisterMenu()
     {
+        //--
+        JPanel Register_Panel = new JPanel(new GridBagLayout());
+        Register_Panel.setBackground(Color.BLUE);
+        //--
 
+        //--
+        JPanel User_Name_Panel = new JPanel(new FlowLayout());
+        User_Name_Panel.setOpaque(false);
+
+        JLabel User_Name = new JLabel("Username:");
+        User_Name.setFont(new Font(Font.SERIF, Font.BOLD, 25));
+        User_Name_Panel.add(User_Name);
+
+        JTextField User_Name_Field = new JTextField(15);
+        User_Name_Field.setPreferredSize(new Dimension(0, 30));
+        User_Name_Panel.add(User_Name_Field);
+
+        Register_Panel.add(User_Name_Panel, setConstraints(1, 1, 0 ,0));
+        //--
+
+        //--
+        JPanel First_Name_Panel = new JPanel(new FlowLayout());
+        First_Name_Panel.setOpaque(false);
+
+        JLabel First_Name = new JLabel("First Name:");
+        First_Name.setFont(new Font(Font.SERIF, Font.BOLD, 25));
+        First_Name_Panel.add(First_Name);
+
+        JTextField First_Name_Field = new JTextField(15);
+        First_Name_Field.setPreferredSize(new Dimension(0, 30));
+        First_Name_Panel.add(First_Name_Field);
+
+        Register_Panel.add(First_Name_Panel, setConstraints(1, 1, 0 ,1));
+        //--
+
+        //--
+        JPanel Last_Name_Panel = new JPanel(new FlowLayout());
+        Last_Name_Panel.setOpaque(false);
+
+        JLabel Last_Name = new JLabel("Last Name:");
+        Last_Name.setFont(new Font(Font.SERIF, Font.BOLD, 25));
+        Last_Name_Panel.add(Last_Name);
+
+        JTextField Last_Name_Field = new JTextField(15);
+        Last_Name_Field.setPreferredSize(new Dimension(0, 30));
+        Last_Name_Panel.add(Last_Name_Field);
+
+        Register_Panel.add(Last_Name_Panel, setConstraints(1, 1, 0 ,2));
+        //--
+
+
+        //--
+        JPanel Password_Panel = new JPanel(new FlowLayout());
+        Password_Panel.setOpaque(false);
+
+        JLabel Password = new JLabel("Password:");
+        Password.setFont(new Font(Font.SERIF, Font.BOLD, 25));
+        Password_Panel.add(Password);
+
+        JPasswordField Password_Field = new JPasswordField(15);
+        Password_Field.setPreferredSize(new Dimension(0, 30));
+        Password_Panel.add(Password_Field);
+
+        Register_Panel.add(Password_Panel,  setConstraints(1,1,0,3));
+        //--
+
+        //--
+        JButton LogIn_Button = new JButton("Register");
+        LogIn_Button.setPreferredSize(new Dimension(300, 30));
+        LogIn_Button.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                try
+                {
+                    Session.Register(User_Name_Field.getText(), First_Name_Field.getText(), Last_Name_Field.getText(),Password_Field.getText());
+                }
+                catch (SQLException ex)
+                {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        Register_Panel.add(LogIn_Button, setConstraints(1,1,0,4));
+        //--
+
+        Window.add(Register_Panel);
+        Window.setVisible(true);
     }
 
 }
