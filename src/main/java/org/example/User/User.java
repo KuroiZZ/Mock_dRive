d43c9dc59dfa1dc4215e41d793b04ab043dc33d5
KuroiZZ <habiltatar01@gmail.com>
2024-12-24 21:04:02 +0300

    CreateTeam function changed to new table system. GetAllTeams function changed. Action added to teams buttons.

--- src/main/java/org/example/User/User.java
+++ src/main/java/org/example/User/User.java
@@ -1,9 +1,12 @@
 package org.example.User;
 
 import org.example.Connection;
+import org.example.Main;
 import org.example.SessionSystem.Session;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -57,13 +60,20 @@ public class User
 
             if (Teammate_Id != null)
             {
-                String query = "INSERT INTO team (Team_Id, Team_Name, Team_Leader, Team_Member) VALUES(?, ?,?,?);";
+                String query = "INSERT INTO team (Team_Id, Team_Name, Team_Leader) VALUES(?, ?, ?);";
+                String Team_Id = UUID.randomUUID().toString();
 
                 stmt = connection.prepareStatement(query);
-                stmt.setString(1, UUID.randomUUID().toString());
+                stmt.setString(1, Team_Id);
                 stmt.setString(2, TeamName);
-                stmt.setString(3, this.UserId);
-                stmt.setString(4, Teammate_Id);
+                stmt.setString(3, this.UserName);
+                stmt.execute();
+
+                query = "INSERT INTO team_member (Team_Id, Team_Member) VALUES(?, ?);";
+
+                stmt = connection.prepareStatement(query);
+                stmt.setString(1, Team_Id);
+                stmt.setString(2, TeammateName);
 
                 stmt.execute();
                 stmt.close();
@@ -78,13 +88,52 @@ public class User
 
         ArrayList<JButton> Teams = new ArrayList<JButton>();
 
-        String query = "SELECT Team_Name FROM team WHERE Team_Leader = ?";
+        String query = "SELECT Team_Name, Team_Id FROM team WHERE Team_Leader = ?";
         PreparedStatement stmt = connection.prepareStatement(query);
-        stmt.setString(1, this.UserId);
+        stmt.setString(1, this.UserName);
         ResultSet rs = stmt.executeQuery();
         while (rs.next())
         {
             JButton new_button = new JButton(rs.getString(1));
+            String Team_Id = rs.getString(2);
+            new_button.addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+
+                    try
+                    {
+                        ArrayList<User> user_list = new ArrayList<>();
+
+                        String query1 = "SELECT * FROM team WHERE Team_Id = ?";
+                        PreparedStatement stmt1 = connection.prepareStatement(query1);
+                        stmt1.setString(1, Team_Id);
+                        ResultSet rs1 = stmt1.executeQuery();
+                        if (rs1.next())
+                        {
+                            System.out.println(rs1.getString(3));
+                            String query2 = "SELECT Team_Member FROM team_member WHERE Team_Id = ?";
+                            PreparedStatement stmt2 = connection.prepareStatement(query2);
+                            stmt2.setString(1, Team_Id);
+                            ResultSet rs2 = stmt2.executeQuery();
+                            while (rs2.next())
+                            {
+                                User new_user = Session.SelectUser(rs2.getString(1));
+                                user_list.add(new_user);
+                            }
+                            Main.current_team = new Team(rs1.getString(1), rs1.getString(2), Session.SelectUser(rs1.getString(3)), user_list);
+                            System.out.println(Main.current_team);
+                        }
+
+
+                    }
+                    catch (SQLException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                }
+            });
             Teams.add(new_button);
         }
         return Teams;
