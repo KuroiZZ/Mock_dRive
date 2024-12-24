f5affccbf1a5e82f1b23095b13822d3b2c613642
KuroiZZ <habiltatar01@gmail.com>
2024-12-24 21:01:46 +0300

    current_team global variable created for instant selected team.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import org.example.GUI.GUI_Elements;
+import org.example.User.Team;
 import org.example.User.User;
 
 public class Main
 {
     static public User current_user;
+    static public Team current_team;
 
     public static void main(String[] args)
     {
